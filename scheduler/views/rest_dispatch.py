fd50850237cb478b4893baccf04a39b4e05e4389
mike seibel <mikes@washington.edu>
2015-10-02 13:43:22 -0700

    adds oauth api validation

--- scheduler/views/rest_dispatch.py
+++ scheduler/views/rest_dispatch.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.http import HttpResponse
 from authz_group import Group
+from blti import BLTI, BLTIException
 from blti.views.rest_dispatch import RESTDispatch as BLTIRESTDispatch
 from blti.views.rest_dispatch import RESTDispatchAuthorization
 import simplejson as json
@@ -14,8 +15,13 @@ class RESTDispatch(BLTIRESTDispatch):
         try:
             self.blti_authorize(request)
         except RESTDispatchAuthorization:
-            if (not (request.user.is_authenticated() and
-                     Group().is_member_of_group(
-                         request.user.username,
-                         settings.PANOPTO_ADMIN_GROUP))):
-                raise
+            try:
+                BLTI().oauth_validate(request)
+            except BLTIException:
+                if (request.user.is_authenticated() and
+                    Group().is_member_of_group(
+                        request.user.username,
+                        settings.PANOPTO_ADMIN_GROUP)):
+                    return
+
+                raise RESTDispatchAuthorization("Access Denied")
