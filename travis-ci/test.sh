4db446baac6c2c8f1a3688184f0c5a0518f175de
mike seibel <mikes@washington.edu>
2020-02-27 13:21:20 -0800

    panopt-client from pypi, coveragerc

--- travis-ci/test.sh
+++ travis-ci/test.sh
@@ -29,9 +29,9 @@ elif [ -d ${DJANGO_APP}/static/js ]; then
     run_test "jshint ${DJANGO_APP}/static/js --verbose"
 fi
 
-run_test "coverage run --rcfile=.coveragerc --source=${DJANGO_APP} '--omit=*/migrations/*' manage.py test ${DJANGO_APP}"
+run_test "coverage run --rcfile=/app/.coveragerc --source=${DJANGO_APP} '--omit=*/migrations/*' manage.py test ${DJANGO_APP}"
 
 # put generaged coverage result where it will get processed
-cp .coverage* /coverage
+cp .coverage.* /coverage
 
 exit 0
