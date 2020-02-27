6e8373353617be51e46767f65a2c307e201bea29
mike seibel <mikes@washington.edu>
2020-02-26 16:17:40 -0800

    tests

---
+++ scheduler/test/__init__.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import User
+from uw_gws.utilities import fdao_gws_override
+from uw_pws.util import fdao_pws_override
+
+
+def get_user(username):
+    try:
+        user = User.objects.get(username=username)
+        return user
+    except Exception as ex:
+        user = User.objects.create_user(username, password='pass')
+        return user
+
+
+def get_user_pass(username):
+    return 'pass'
