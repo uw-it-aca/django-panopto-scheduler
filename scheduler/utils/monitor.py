a6d20f0eddd547e219702dcab9af20f9f7a0327c
mike seibel <mikes@washington.edu>
2021-10-28 13:16:41 -0700

    forgotten module

---
+++ scheduler/utils/monitor.py
@@ -0,0 +1,15 @@
+# Copyright 2021 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
+from uw_gws import GWS
+
+
+def email_addresses_from_group(group_name):
+    email = []
+    gws = GWS()
+
+    for netid in gws.get_effective_members(group_name):
+        email.append("{}.uw.edu".format(netid))
+
+    return email
+    
