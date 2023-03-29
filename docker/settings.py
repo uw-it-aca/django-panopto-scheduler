5be5c05fae6ef8a002d6524f63b2e2ec8b2834c9
mike seibel <mikes@washington.edu>
2023-03-28 18:08:50 -0700

    licence date bump

--- docker/settings.py
+++ docker/settings.py
@@ -1,3 +1,6 @@
+# Copyright 2023 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
 from .base_settings import *
 import json
 
--- docker/urls.py
+++ docker/urls.py
@@ -1,3 +1,6 @@
+# Copyright 2023 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
 from .base_urls import *
 from django.conf import settings
 from django.conf.urls import include, re_path
