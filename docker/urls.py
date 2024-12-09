3a54021be32bde7e54267d9ceaadb685220d12cc
mike seibel <mikes@washington.edu>
2024-12-09 09:44:32 -0800

    lti 1.3 auth urls

--- docker/urls.py
+++ docker/urls.py
@@ -10,13 +10,8 @@ from django.views.i18n import JavaScriptCatalog
 
 urlpatterns += [
     re_path(r'^', include('scheduler.urls')),
+    re_path(r'^blti/', include('blti.urls')),
     re_path(r'^restclients/', include('rc_django.urls')),
     re_path(r'^jsi18n/$', JavaScriptCatalog.as_view(packages=['scheduler']),
             name='javascript-catalog'),
 ]
-
-if (getattr(settings, 'LTI_DEVELOP_APP', None)
-        and getattr(settings, "DEBUG", False)):
-    urlpatterns += [
-        re_path(r'^blti/', include('blti.urls')),
-    ]
